e746a9465d1f11dc8202428c5eb1f0eeab7cb194
longWorm <arukolov@yandex.ru>
2017-09-09 00:13:51 +1000

    trulala

---
+++ alonePlanetUnity/Assets/GameObjectsManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using UnityEngine;
+using System.Globalization;
+
+namespace alonePlanetUnity.Assets
+{
+    public class GameObjectsManager
+    {
+        public GameObject _planet;
+        public GameObject[] _stars;
+		
+        public static float Delta = 5F;
+        public static float DeltaSpeedOnUserInput = 1F;
+            
+        public Vector2 planetSpeed = new Vector2(0, 0);
+        public Vector2 _force = new Vector2(0, 0);
+
+		public Vector3 PlanetInitialPars;
+
+        public GameObjectsManager(GameObject planet, GameObject starPrefab)
+        {
+            _planet = planet;
+            var path = System.IO.Path.Combine(Application.streamingAssetsPath, "level1.xml");
+            var content = System.IO.File.ReadAllText(path);
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(content);
+
+            var stars = GetStars(ref xmldoc);
+            var planetPars = GetPlanet(ref xmldoc);
+            PlanetInitialPars = _planet.transform.position = new Vector3(planetPars.x, planetPars.y, 1f);
+            _planet.transform.localScale = new Vector3(planetPars.r, planetPars.r, planetPars.r);
+            var i = 0;
+            _stars = new GameObject[stars.GetLength(0)];
+            foreach (var star in stars)
+            {
+                _stars[i] = GameObject.Instantiate(starPrefab, new Vector3(star.x, star.y, 5.0f), Quaternion.identity);
+                _stars[i].transform.position = new Vector3(star.x, star.y, 1f);
+                _stars[i].transform.localScale = new Vector3(star.r, star.r, star.r);
+                _stars[i].SetActive(true);
+                i++;
+            }
+        }
+
+        protected struct Circle
+        {
+            public float x, y, r;
+        }
+
+
+        private Circle GetCircleFromNode(XmlNode node)
+        {
+            Circle result = new Circle();
+			result.x = float.Parse(node.Attributes.GetNamedItem("x").Value, CultureInfo.InvariantCulture);
+			result.y = float.Parse(node.Attributes.GetNamedItem("y").Value, CultureInfo.InvariantCulture);
+			result.r = float.Parse(node.Attributes.GetNamedItem("r").Value, CultureInfo.InvariantCulture);
+            return result;
+		}
+
+        private Circle[] GetStars(ref XmlDocument xmldoc)
+        {
+			XmlNodeList stars = xmldoc.SelectNodes("/Body/Stars/Star");
+            Circle[] result = new Circle[stars.Count];
+            int i = 0;
+			foreach (XmlNode star in stars)
+			{
+                result[i] = GetCircleFromNode(star);
+                i++;
+			}
+            return result;
+		}
+
+        private Circle GetPlanet(ref XmlDocument xmldoc)
+        {
+            XmlNode planet = xmldoc.SelectSingleNode("/Body/Planet");
+            return GetCircleFromNode(planet);
+        }
+
+        public void LoadLevel()
+        {
+            
+        }
+    }
+}
---
+++ alonePlanetUnity/Assets/main.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using alonePlanetUnity.Assets;
+using UnityEngine;
+
+public class main : MonoBehaviour {
+    private GameObjectsManager _manager;
+	public GameObject _planet;
+    public GameObject _starPrefab;
+	
+    void Start () {
+        _manager = new GameObjectsManager(_planet,_starPrefab);
+		_planet.transform.position = _manager.PlanetInitialPars;
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+		var ps = _planet.GetComponent<ParticleSystem>();
+        ParticleSystem.EmitParams emitOverride = new ParticleSystem.EmitParams();
+		emitOverride.startLifetime = 20f;
+		ps.Emit(emitOverride, 20);
+
+        Invoke("Respawn", 0.5f);
+    }
+
+    private void Respawn()
+    {
+		_planet.transform.position = _manager.PlanetInitialPars;
+        
+    }
+	void Update () {
+        UpdateForce();
+	}
+
+    private void UpdateForce()
+    {
+        var force = new Vector3(0, 0, 0);
+        ProcessUserInput(ref force);
+        ProcessStarsInteractions(ref force);
+		_planet.GetComponent<ConstantForce>().force = new Vector3(force.x, force.y, 0.0f);
+    }
+
+    private void ProcessUserInput(ref Vector3 force)
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            force.x -= GameObjectsManager.DeltaSpeedOnUserInput;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            force.x += GameObjectsManager.DeltaSpeedOnUserInput;
+        if (Input.GetKey(KeyCode.UpArrow))
+            force.y += GameObjectsManager.DeltaSpeedOnUserInput;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            force.y -= GameObjectsManager.DeltaSpeedOnUserInput;
+        else if (Input.GetKey(KeyCode.R))
+			_planet.transform.position = _manager.PlanetInitialPars;
+    }
+
+    private void ProcessStarsInteractions(ref Vector3 force)
+    {
+        float deltaX = 0, deltaY = 0;
+        foreach (var star in _manager._stars)
+        {
+            float distance = Vector3.Distance(_planet.transform.position, star.transform.position);
+            float attractionForce = GameObjectsManager.Delta / Mathf.Pow(distance, 2);
+
+            float xConstituent = star.transform.position.x - _planet.transform.position.x;
+            float yConstituent = star.transform.position.y - _planet.transform.position.y;
+            
+            deltaX += (xConstituent / distance) * attractionForce;
+            deltaY += (yConstituent / distance) * attractionForce;
+        }
+        //Debug.Log("forceX=" + deltaX + " forceY=" + deltaY);
+        force.x += deltaX; 
+        force.y += deltaY;
+    }
+}
