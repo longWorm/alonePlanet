d76a64565f3dce77bb214b25aea0e795186be5fd
longWorm <arukolov@yandex.ru>
2017-09-10 23:25:22 +1000

    added coins objects, added text with current planet coordinates, some refactoringa

--- alonePlanetUnity/Assets/GameObjectsManager.cs
+++ alonePlanetUnity/Assets/GameObjectsManager.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Xml;
 using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Globalization;
 
 namespace alonePlanetUnity.Assets
 {
-    public class GameObjectsManager
+    public class GameObjectsManager : MonoBehaviour
     {
         public GameObject _planet;
-        public GameObject[] _stars;
+		public GameObject[] _stars;
+		public GameObject[] _coins;
 		
         public static float Delta = 5F;
         public static float DeltaSpeedOnUserInput = 1F;
-            
-        public Vector2 planetSpeed = new Vector2(0, 0);
-        public Vector2 _force = new Vector2(0, 0);
 
-		public Vector3 PlanetInitialPars;
+        private Vector3 _planetInitialPars;    
+        public Vector3 PlanetInitialPars{
+            get { return _planetInitialPars; }
+            private set { _planetInitialPars = value; }
+        }
 
-        public GameObjectsManager(GameObject planet, GameObject starPrefab)
+        public GameObjectsManager(GameObject planet, GameObject starPrefab, GameObject coinPrefab)
         {
             _planet = planet;
             var path = System.IO.Path.Combine(Application.streamingAssetsPath, "level1.xml");
@@ -28,20 +31,45 @@ namespace alonePlanetUnity.Assets
             XmlDocument xmldoc = new XmlDocument();
             xmldoc.LoadXml(content);
 
-            var stars = GetStars(ref xmldoc);
             var planetPars = GetPlanet(ref xmldoc);
             PlanetInitialPars = _planet.transform.position = new Vector3(planetPars.x, planetPars.y, 1f);
             _planet.transform.localScale = new Vector3(planetPars.r, planetPars.r, planetPars.r);
-            var i = 0;
+
+            var stars = GetStars(ref xmldoc);
             _stars = new GameObject[stars.GetLength(0)];
+			var i = 0;
             foreach (var star in stars)
             {
-                _stars[i] = GameObject.Instantiate(starPrefab, new Vector3(star.x, star.y, 5.0f), Quaternion.identity);
-                _stars[i].transform.position = new Vector3(star.x, star.y, 1f);
-                _stars[i].transform.localScale = new Vector3(star.r, star.r, star.r);
-                _stars[i].SetActive(true);
+                _stars[i] = CreateGO(starPrefab, star);
                 i++;
             }
+
+            var coins = GetCoins(ref xmldoc);
+            _coins = new GameObject[coins.GetLength(0)];
+			i = 0;
+			foreach (var coin in coins)
+			{
+                _coins[i] = CreateGO(coinPrefab, coin);
+				i++;
+			}
+		}
+
+        public void DeleteCoin(GameObject coin)
+        {
+            int index = 0;
+            foreach (var coinIt in _coins)
+            {
+                if (coinIt == coin)
+                {
+                    var tmp = new List<GameObject>(_coins);
+                    tmp.RemoveAt(index);
+                    _coins = tmp.ToArray();
+                    break;
+                }
+                index++;
+            }
+
+			Destroy(coin);
         }
 
         protected struct Circle
@@ -49,8 +77,16 @@ namespace alonePlanetUnity.Assets
             public float x, y, r;
         }
 
+        private static GameObject CreateGO(GameObject prefab, Circle par)
+        {
+            var result = GameObject.Instantiate(prefab, new Vector3(par.x, par.y, 5.0f), Quaternion.identity);
+			result.transform.position = new Vector3(par.x, par.y, 1f);
+			result.transform.localScale = new Vector3(par.r, par.r, par.r);
+			result.SetActive(true);
+            return result;
+		}
 
-        private Circle GetCircleFromNode(XmlNode node)
+        private static Circle GetCircleFromNode(XmlNode node)
         {
             Circle result = new Circle();
 			result.x = float.Parse(node.Attributes.GetNamedItem("x").Value, CultureInfo.InvariantCulture);
@@ -59,7 +95,7 @@ namespace alonePlanetUnity.Assets
             return result;
 		}
 
-        private Circle[] GetStars(ref XmlDocument xmldoc)
+        private static Circle[] GetStars(ref XmlDocument xmldoc)
         {
 			XmlNodeList stars = xmldoc.SelectNodes("/Body/Stars/Star");
             Circle[] result = new Circle[stars.Count];
@@ -72,15 +108,23 @@ namespace alonePlanetUnity.Assets
             return result;
 		}
 
-        private Circle GetPlanet(ref XmlDocument xmldoc)
+		private static Circle[] GetCoins(ref XmlDocument xmldoc)
+		{
+			XmlNodeList coins = xmldoc.SelectNodes("/Body/Coins/Coin");
+			Circle[] result = new Circle[coins.Count];
+			int i = 0;
+			foreach (XmlNode coin in coins)
+			{
+				result[i] = GetCircleFromNode(coin);
+				i++;
+			}
+			return result;
+		}
+
+        private static Circle GetPlanet(ref XmlDocument xmldoc)
         {
             XmlNode planet = xmldoc.SelectSingleNode("/Body/Planet");
             return GetCircleFromNode(planet);
-        }
-
-        public void LoadLevel()
-        {
-            
         }
     }
 }
--- alonePlanetUnity/Assets/main.cs
+++ alonePlanetUnity/Assets/main.cs
@@ -6,29 +6,47 @@ using UnityEngine;
 public class main : MonoBehaviour {
     private GameObjectsManager _manager;
 	public GameObject _planet;
-    public GameObject _starPrefab;
-	
-    void Start () {
-        _manager = new GameObjectsManager(_planet,_starPrefab);
-		_planet.transform.position = _manager.PlanetInitialPars;
+	public GameObject _starPrefab;
+    public GameObject _coinPrefab;
+    public GameObject _planetCoordinatesText;
 
+    void Start () {
+        _manager = new GameObjectsManager(_planet,_starPrefab,_coinPrefab);
+		_planet.transform.position = _manager.PlanetInitialPars;
     }
 
     void OnCollisionEnter(Collision col)
     {
-		var ps = _planet.GetComponent<ParticleSystem>();
-        ParticleSystem.EmitParams emitOverride = new ParticleSystem.EmitParams();
-		emitOverride.startLifetime = 20f;
-		ps.Emit(emitOverride, 20);
+        if (col.gameObject.tag == "star")
+        {
+			var ps = _planet.GetComponent<ParticleSystem>();
+			ParticleSystem.EmitParams emitOverride = new ParticleSystem.EmitParams();
+			emitOverride.startLifetime = 20f;
+			ps.Emit(emitOverride, 20);
+			Invoke("Respawn", 0.5f);
+		}
+        else if (col.gameObject.tag == "coin")
+        {
+            var ps = col.gameObject.GetComponent<ParticleSystem>();
+			ParticleSystem.EmitParams emitOverride = new ParticleSystem.EmitParams();
+			emitOverride.startLifetime = 20f;
+			ps.Emit(emitOverride, 20);
 
-        Invoke("Respawn", 0.5f);
+            StartCoroutine(DestroyCoin(col.gameObject, 0.5f));
+        }
+    }
+
+    private IEnumerator DestroyCoin(GameObject coin, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _manager.DeleteCoin(coin);
     }
 
     private void Respawn()
     {
 		_planet.transform.position = _manager.PlanetInitialPars;
-        
     }
+
 	void Update () {
         UpdateForce();
 	}
@@ -39,6 +57,13 @@ public class main : MonoBehaviour {
         ProcessUserInput(ref force);
         ProcessStarsInteractions(ref force);
 		_planet.GetComponent<ConstantForce>().force = new Vector3(force.x, force.y, 0.0f);
+        UpdatePlanetCoordinatesText();
+    }
+
+    private void UpdatePlanetCoordinatesText()
+    {
+        var textMesh = _planetCoordinatesText.GetComponent<TextMesh>();
+        textMesh.text = string.Format("x:{0:0.000} y:{1:0.000}", _planet.transform.position.x, _planet.transform.position.y);
     }
 
     private void ProcessUserInput(ref Vector3 force)
@@ -69,7 +94,7 @@ public class main : MonoBehaviour {
             deltaX += (xConstituent / distance) * attractionForce;
             deltaY += (yConstituent / distance) * attractionForce;
         }
-        //Debug.Log("forceX=" + deltaX + " forceY=" + deltaY);
+
         force.x += deltaX; 
         force.y += deltaY;
     }
