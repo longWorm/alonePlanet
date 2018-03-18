786e64a667f2040879544dae7a1a1bc12b287e97
longWorm <arukolov@yandex.ru>
2018-03-19 00:59:38 +1100

    main menu redesign

--- alonePlanetUnity/Assets/scripts/MainMenu.cs
+++ alonePlanetUnity/Assets/scripts/MainMenu.cs
@@ -5,85 +5,29 @@ using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject _startGame;
+    public GameObject _continueGame;
     public GameObject _selectLevel;
-    public GameObject _selectLevelView;
-    public GameObject _quit;
-    public GameObject _buttonTemplate;
-    public GameObject _contentPtr;
-    public Animator _animator;
-    public GameObject _levelButtonsContainer;
+	public GameObject _quit;
 
-    private bool _levelListShown = false;
-    private List<string> _levelList = new List<string>();
-
-
-	void Start()
+    void Start()
 	{
-        _startGame.GetComponent<Button>().onClick.AddListener(StartGame);
+        _continueGame.GetComponent<Button>().onClick.AddListener(ContinueGame);
         _selectLevel.GetComponent<Button>().onClick.AddListener(SelectLevel);
         _quit.GetComponent<Button>().onClick.AddListener(Quit);
-        RectTransform rect = _levelButtonsContainer.GetComponent<RectTransform>();
-        Debug.Log(rect.rect.width);
-        _contentPtr.GetComponent<GridLayoutGroup>().cellSize = new Vector2(100
-                                                                                      ,100);
+    }
 
-        _levelList.Add("1");
-        _levelList.Add("2");
-        _levelList.Add("3");
-        _levelList.Add("4");
-        _levelList.Add("5");
-        _levelList.Add("6");
-        _levelList.Add("7");
-        _levelList.Add("8");
-        //_levelList.Add("3");
-        //_levelList.Add("4");
-        //_levelList.Add("5");
-        //_levelList.Add("6");
-        //_levelList.Add("1");
-        //_levelList.Add("2");
-        //_levelList.Add("3");
-        //_levelList.Add("4");
-        //_levelList.Add("5");
-        //_levelList.Add("6");
-
-        foreach (var level in _levelList)
-        {
-            GameObject button = Instantiate(_buttonTemplate) as GameObject;
-            button.GetComponent<Button>().onClick.AddListener(delegate { Button_Click("1"); });
-            button.GetComponentInChildren<Text>().text = level;
-            button.transform.SetParent(_contentPtr.transform, false);
-            //button.transform.SetParent(_selectLevelView1.transform);
-        }
-        rect = (RectTransform)_levelButtonsContainer.transform;
-        Debug.Log(rect.rect.width);
-       
-	}
-
-    private void StartGame()
+    private void ContinueGame()
     {
         SceneManager.LoadScene("mainScene");
     }
 
     private void SelectLevel()
     {
-        if (_levelListShown)
-            _animator.SetTrigger("hide");
-        else
-            _animator.SetTrigger("show");
-		_levelListShown = !_levelListShown;
-
-        //_selectLevelView.GetComponent<Canvas>().enabled = !_selectLevelView.GetComponent<Canvas>().enabled;
+        SceneManager.LoadScene("selectLevel");
     }
 
     private void Quit()
     {
         Application.Quit();
-    }
-
-    static public void Button_Click(string level)
-    {
-        PlayerPrefs.SetString("level", level);
-        SceneManager.LoadScene("mainScene");
     }
 }
---
+++ alonePlanetUnity/Assets/scripts/SelectLevel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SelectLevel : MonoBehaviour {
+
+    public GameObject _contentPtr;
+    public GameObject _buttonTemplate;
+
+    void Start () {
+        List<string> levelList = new List<string>();
+
+        levelList.Add("1");
+        levelList.Add("2");
+        levelList.Add("3");
+        levelList.Add("4");
+        levelList.Add("5");
+        levelList.Add("6");
+        levelList.Add("7");
+        levelList.Add("8");
+    
+        foreach (var level in levelList) {
+            GameObject button = Instantiate(_buttonTemplate) as GameObject;
+            button.GetComponent<Button>().onClick.AddListener(delegate { ButtonClick(level); });
+            button.GetComponentInChildren<Text>().text = level;
+            button.transform.SetParent(_contentPtr.transform, false);
+        }
+
+	}
+
+    private void ButtonClick(string level) {
+        Debug.Log(level);
+        Debug.Log("clicked");
+        PlayerPrefs.SetString("level", level);
+        SceneManager.LoadScene("mainScene");
+    }
+}
