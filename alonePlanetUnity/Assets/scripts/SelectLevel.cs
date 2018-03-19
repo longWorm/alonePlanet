299ac441d9ad0a628341c43ef987953ffd784ddb
longWorm <arukolov@yandex.ru>
2018-03-20 01:53:36 +1100

    loading levels list from xml file

--- alonePlanetUnity/Assets/scripts/SelectLevel.cs
+++ alonePlanetUnity/Assets/scripts/SelectLevel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Xml;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,32 +11,61 @@ public class SelectLevel : MonoBehaviour {
 
     public GameObject _contentPtr;
     public GameObject _buttonTemplate;
+    public GameObject _backToMainMenuButton;
+
+#if UNITY_ANDROID
+
+    IEnumerator WaitForWWW(WWW www)
+    {
+        yield return www;
+    }
+
+    private string LoadFile(string fileName)
+    {
+        var path = "jar:file://" + Application.dataPath + "!/assets/" + fileName;
+        WWW www = new WWW(path);
+        StartCoroutine(WaitForWWW(www));
+        while (!www.isDone) { }
+        return www.text;
+    }
+#endif
+
+#if UNITY_STANDALONE_OSX
+    private string LoadFile(string fileName)
+    {
+        var path = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+        var content = System.IO.File.ReadAllText(path);
+        return content;
+    }
+#endif
 
     void Start () {
-        List<string> levelList = new List<string>();
-
-        levelList.Add("1");
-        levelList.Add("2");
-        levelList.Add("3");
-        levelList.Add("4");
-        levelList.Add("5");
-        levelList.Add("6");
-        levelList.Add("7");
-        levelList.Add("8");
-    
-        foreach (var level in levelList) {
-            GameObject button = Instantiate(_buttonTemplate) as GameObject;
-            button.GetComponent<Button>().onClick.AddListener(delegate { ButtonClick(level); });
-            button.GetComponentInChildren<Text>().text = level;
-            button.transform.SetParent(_contentPtr.transform, false);
-        }
-
+        LoadLevels();
+        _backToMainMenuButton.GetComponent<Button>().onClick.AddListener(delegate { BackToMainMenu(); });
 	}
 
     private void ButtonClick(string level) {
-        Debug.Log(level);
-        Debug.Log("clicked");
         PlayerPrefs.SetString("level", level);
         SceneManager.LoadScene("mainScene");
+    }
+
+    private void BackToMainMenu() {
+        SceneManager.LoadScene("mainMenu");
+    }
+
+    private void LoadLevels()
+    {
+        var content = LoadFile("levelList.xml");
+        XmlDocument xmldoc = new XmlDocument();
+        xmldoc.LoadXml(content);
+        XmlNodeList levels = xmldoc.SelectNodes("/levels/level");
+        foreach (XmlNode level in levels)
+        {
+            GameObject button = Instantiate(_buttonTemplate) as GameObject;
+            button.GetComponent<Button>().onClick.AddListener(delegate { ButtonClick(level.Attributes.GetNamedItem("file").Value); });
+            button.GetComponentInChildren<Text>().text = level.Attributes.GetNamedItem("name").Value;
+            button.transform.SetParent(_contentPtr.transform, false);
+            button.GetComponent<Button>().interactable = Convert.ToBoolean(level.Attributes.GetNamedItem("enabled").Value);
+        }
     }
 }
